57a010faa921136b6eaa28a46546dc7fcaff49ae
Lyndsey Clarkson <lyndseydj1808@gmail.com>
2026-03-14 22:46:28 -0500

    form added to parent dashboard

--- unit-1-project/src/App.jsx
+++ unit-1-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,13 +7,21 @@ import FeelingFriends from './games/FeelingFriends/FeelingFriends';
 import MainGames from './pages/MainGames';
 
 function App () {
+
+  const [childName, setChildName] = useState("");
+  const [childAge, setChildAge] = useState("");
+
+  const updateChild = (name, age) => {
+    setChildName(name);
+    setChildAge(age);
+  }
    return (
        <>
            <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/mini-games" element={<MainGames/>} />
+            <Route path="/mini-games" element={<MainGames childName={childName}/>} />
             <Route path="/games/feeling-friends" element={<FeelingFriends/>} />
-            <Route path="/parent-dashboard" element={<ParentDashboard/>}/>
+            <Route path="/parent-dashboard" element={<ParentDashboard onUpdate={updateChild}/>}/>
             <Route path="/about" element={<About/>}/>
            </Routes>
        </>
--- unit-1-project/src/pages/ParentDashboard.jsx
+++ unit-1-project/src/pages/ParentDashboard.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import BackButton from "../components/BAckButton";
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
 
-export default function ParentDashboard() {
+export default function ParentDashboard({ onUpdate }) {
+
+    const [inputName, setInputName] = useState("");
+    const [inputAge, setInputAge] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onUpdate(inputName, inputAge);
+    }
   return (
     <div>
       <h1>Hi Parents! Welcome to Little Creatures Feel Big!</h1>
@@ -16,12 +22,26 @@ export default function ParentDashboard() {
         experience the same highs and lows kids do. Name That Feeling: Practice
         identifying emotions through play. Build a Toolkit: Learn simple,
         effective ways to regulate those "big feelings" in a safe, stress-free
-        environment. Let’s get started! Please enter your child’s details below
+        environment. Let's get started! Please enter your child's details below
         so our creatures can greet them by name and tailor the experience to
         their age.
       </p>
-      <form>Form here!!!</form>
-      <BackButton/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputName}
+          onChange={(event) => setInputName(event.target.value)}
+          placeholder="Enter child's name"
+        />
+        <input
+        type="number"
+        value={inputAge}
+        onChange= {(event) => setInputAge(event.target.value)}
+        placeholder= "Enter child's age"
+        />
+        <button type="submit">Save</button>
+      </form>
+      <BackButton />
     </div>
   );
 }
