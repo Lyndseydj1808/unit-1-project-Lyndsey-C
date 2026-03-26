d284edc00c57d3fe928216ee1867ecd28fa0012c
Lyndsey Clarkson <lyndseydj1808@gmail.com>
2026-03-25 23:00:12 -0500

    added and styled home button

--- unit-1-project/src/components/BackButton.jsx
+++ unit-1-project/src/components/BackButton.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 //back button component to go back to previous page
 export default function BackButton() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
   return (
-
     <div>
-      <button className="back-button" onClick={() => navigate(-1)}>Back</button>
+      <button className="back-button" onClick={() => navigate(-1)}>
+        Back
+      </button>
     </div>
-  )
+  );
 }
---
+++ unit-1-project/src/components/HomeButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function HomeButton() {
+
+  return <div>
+    <Link className="home-button" to="/">Home 🏠 </Link>
+  </div>;
+}
