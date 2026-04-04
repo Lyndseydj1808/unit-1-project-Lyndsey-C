e5bbfae0d4ae255f7490ea3c5f4f5886c2fcbd41
Lyndsey Clarkson <lyndseydj1808@gmail.com>
2026-04-04 00:38:42 -0500

    updated styling for parent dashboard and about pages

--- unit-1-project/src/pages/About.jsx
+++ unit-1-project/src/pages/About.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BackButton from "../components/BackButton";
-import './About.css'
+import "./About.css";
 
-export default function About() {
+export default function About({ childName }) {
   return (
     <div className="about-container">
       <p className="about-description">
-        Hi freind! Welcome to Little Creatures Feel Big! We hope you have so
-        much fun playing with us! We love learning about our big feelings! You
-        can play our Feeling Friends game to guess how the creatures are feeling
-        today! Parents, check out our parent dashboard for more info and to
-        enter your childs name and age to add to their fun!
+        {childName ? `Hi ${childName}!` : "Hi friend!"} Welcome to Little
+        Creatures Feel Big! We hope you have so much fun playing with us! We
+        love learning about our big feelings! You can play our Feeling Friends
+        game to guess how the creatures are feeling today! Or play Kind
+        Creatures and pick a kind saying for our creatures to say! Parents,
+        check out our parent dashboard for more info and to enter your child's
+        name and age to add to their fun!
       </p>
       <BackButton />
     </div>
