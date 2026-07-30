be0c8aeb51bf78d6a99a5116d16b68c8b8a12783
Lyndseydj1808 <lyndseydj1808@gmail.com>
2026-07-29 23:03:24 -0500

    added new mini-game-little and big talk

---
+++ unit-1-project/src/games/LittleAndBigTalk/LittleAndBigTalk.jsx
@@ -0,0 +1,9 @@
+//A conversation starter game for all family members to play together. A question is displayed and player types (or says) there answer. The game gives feedback.
+import React from "react";
+import { littleAndBigQuestions } from "./LittleAndBigQuestions";
+import { useState } from "react";
+import shuffleArray from "../../utils/shuffleArray";
+
+export default LittleAndBigTalk() {
+
+};
